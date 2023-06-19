e21881cdc583b2cae6dea3abc275a18acb7120f1
project <enimesentertainment@gmail.com>
2023-06-19 19:34:00 +0530

    Initial commit

---
+++ WebApplication2/src/java/connection/MyConnection.java
@@ -0,0 +1,26 @@
+ 
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import utils.SystemConstants;
+
+
+public class MyConnection {
+    
+    static Connection con = null; 
+    
+    public static Connection myConnection()
+  {
+    try
+    {
+       Class.forName(SystemConstants.DRIVER);
+       con = DriverManager.getConnection(SystemConstants.URL,SystemConstants.DBUSERNAME,SystemConstants.DBPASSWORD); 
+    }
+    catch(Exception ex)
+    {
+       System.out.println("Connection Exception : "+ex.toString());
+    }
+      return con;
+   }
+}
---
+++ WebApplication2/src/java/utils/SystemConstants.java
@@ -0,0 +1,13 @@
+package utils;
+public class SystemConstants 
+{
+  public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+  public static final String URL = "jdbc:mysql://localhost:3306/music_db";
+  public static final String DBUSERNAME = "root";
+  public static final String DBPASSWORD ="Aditya@92";  
+    
+    
+  public static final int ADMIN = 1;
+  public static final int USER = 2;
+  
+}
